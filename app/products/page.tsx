0d89ea6f1b319f6a0559a04e217f326066342b68
Dinuka Induwara <bandarainduwara3@gmail.com>
2025-09-15 21:00:47 +0530

    Refactor NavBar component layout, update API fetch URLs to use environment variables, enhance Checkout page content, and remove FetchTest component.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,16 +1,19 @@
 import ProductsList from "../productsList";
 
+export const dynamic = 'force-dynamic';
 
 export default async function ProductsPage() {
-    const response= await fetch('http://localhost:3000/api/products');
-    const products= await response.json();
+  const response = await fetch(process.env.NEXT_PUBLIC_SITE_URL + '/api/products');
+  const products = await response.json();
 
-    const response2= await fetch('http://localhost:3000/api/users/2/cart');
-    const cartProducts= await response2.json();
+  const response2 = await fetch(process.env.NEXT_PUBLIC_SITE_URL + '/api/users/2/cart', {
+    cache: 'no-cache',
+  });
+  const cartProducts = await response2.json();
 
-     return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Products</h1>
+  return (
+    <div className="container mx-auto p-8"> 
+      <h1 className="text-4xl font-bold mb-8">Products</h1> 
       <ProductsList products={products} initialCartProducts={cartProducts} />
     </div>
   );
